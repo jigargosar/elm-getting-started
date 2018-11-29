fec2e0aec8c5bc91d042eb06162fd83def75c6e1
Jigar Gosar <jigar.gosar@gmail.com>
2018-11-29 15:16:44 +0530

    view basic styling

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h1, img, text)
+import Html.Attributes exposing (class, src)
+
 
 
 ---- MODEL ----
@@ -36,7 +37,7 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
+    div [ class "sans-serif" ]
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "Your Elm App is working!" ]
         ]
